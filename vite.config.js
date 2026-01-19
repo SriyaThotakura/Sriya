610c757d1b9c6cc0e552dee18a14a5e8b7aa6267
SriyaThotakura <sriyagayathri007@gmail.com>
2026-01-19 13:26:30 -0500

    commi

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      '/assets': path.resolve(__dirname, 'public/assets'),
+    },
+  },
   server: {
     port: 3001,
   },
